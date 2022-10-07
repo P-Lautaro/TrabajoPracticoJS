2ae4e727352f5c2070f9991bc2dbfd74dcb93750
P-Lautaro <parritalauti@gmail.com>
2022-10-06 23:02:01 -0300

    agregando fuction

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,14 @@
-const calcu = ["suma","resta","multiplicacion","division"]
-var monsuma = suma();
+const calcu = ["suma","resta","multiplicacion","division"]   
+var monsuma = suma();     
 let monresta = resta();
 let mondivision = dividir();
 let monmultiplicacion = multiplicar();
 
+
 let inicio;
 
+function sumar(){
+    num1 = 50;
+    num2 = 50;
+    
+}
